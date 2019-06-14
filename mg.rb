0b7b1de8707acfe3f9df0db0d9f32983aa0e8dc4
nissy <ni@sheeda.jp>
2019-06-14 16:29:56 +0900

    v0.0.4

--- mg.rb
+++ mg.rb
@@ -6,7 +6,7 @@ class MG < Formula
   homepage "https://github.com/nissy/mg"
 
   if OS.mac?
-    url "https://github.com/nissy/mg/releases/download/v#{HOMEBREW_VERSION}/mg-#{HOMEBREW_VERSION}_darwin_amd64.tar.gz"
+    url "https://github.com/nissy/mg/releases/download/v#{HOMEBREW_VERSION}/mg-v#{HOMEBREW_VERSION}_darwin_amd64.tar.gz"
     sha256 "5dffc4621fe1790db7296ff8d0b32870b6611dd25383e1afa10eb3d6b1bc2c70"
   end
 
