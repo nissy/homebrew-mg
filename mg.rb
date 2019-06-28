e29970ff6e13847f883fd5bf482a07f095c86e0a
Yoshihiko Nishida <ni@sheeda.jp>
2019-06-28 19:34:02 +0900

    Update mg.rb

--- mg.rb
+++ mg.rb
@@ -1,13 +1,13 @@
 require "formula"
 
-HOMEBREW_VERSION="0.0.4"
+HOMEBREW_VERSION="0.0.5"
 
 class Mg < Formula
   homepage "https://github.com/nissy/mg"
 
   if OS.mac?
     url "https://github.com/nissy/mg/releases/download/v#{HOMEBREW_VERSION}/mg-v#{HOMEBREW_VERSION}_darwin_amd64.tar.gz"
-    sha256 "5dffc4621fe1790db7296ff8d0b32870b6611dd25383e1afa10eb3d6b1bc2c70"
+    sha256 "a2a9fe09fe3662297b073e7182f0ace7b7ef430406a61577810016b6d2eeef4f"
   end
 
   def install
