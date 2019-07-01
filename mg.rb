f753fdaccabb466d57a961d57239bc375d154636
Yoshihiko Nishida <ni@sheeda.jp>
2019-07-01 13:55:02 +0900

    Update mg.rb

--- mg.rb
+++ mg.rb
@@ -8,6 +8,7 @@ class Mg < Formula
   if OS.mac?
     url "https://github.com/nissy/mg/releases/download/v#{HOMEBREW_VERSION}/mg-v#{HOMEBREW_VERSION}_darwin_amd64.tar.gz"
     sha256 "371ba7db8fba0c172054ab9259f8534ae5a93dc641ea6eee270afc9e803fbffe"
+    head "https://github.com/nissy/mg/mg.git"
   end
 
   def install
